22c519d9db062ab487d5f9fdc227ba01444cf2ba
lindelo <u12002862@tuks.co.za>
2015-07-28 12:50:12 +0200

    New Schema

--- database.js
+++ database.js
@@ -49,27 +49,121 @@ var Statistics = new Schema({
 var GynaecologySurgery = new Schema({
 
         ProcedureDate: {type: Date, require: true, title: 'Procedure Date' },
+	
         HospitalNumber: {type: String, require: true, title: 'Hospital Number'},
-        Age: {type:Number, require: true, title: 'Age'},
+	
+        Age: {type: Number, require: true, title: 'Age'},
+	
         Name: {type: String, require: true, title: 'Name'},
+	
         Surname: {type: String, require: true, title: 'Surname'},
-	UnknownField: {Properties: {P: {type: Boolean, title: 'P'}, G: {type: Boolean, title: 'G'}, M: {type: Boolean, title: 'M'}, E: {type: Boolean, title: 'E'}}},
-        HIV: {Status: {type: String, title: 'HIV Status'}, CD4: {type: Number, title: 'CD4'}},
-        ProcedureType: {Properties: {Elective: {type: Boolean, title: 'Elective'}, Emergency: {type: Boolean, title: 'Emergency'}}},
-        Firm: {Properties: {G1: {type: Boolean, title: 'G1'}, G2: {type: Boolean, title: 'G2'}, G3: {type: Boolean, title: 'G3'}, ONC: {type: Boolean, title: 'ONC'}}},
-        ProcedureCat: {Properties: {Oncology: {type: Boolean, title: 'Oncology'}, GeneralGynae: {type: Boolean, title: 'General Gynaecology'}, Infertility: {type: Boolean, title: 'Infertility'}, Urologynaecology: {type: Boolean, title: 'Urologynaecology'}}},
-        AccessType: {Properties: {OpenAbdomen: {type: Boolean, title: 'Open Abdomen'}, Endoscopy: {type: Boolean, title: 'Endoscopy'}, Vaginal: {type: Boolean, title: 'Vaginal'}}},
+	
+	P: {type: Boolean, title: 'P'}, 
+	G: {type: Boolean, title: 'G'}, 
+	M: {type: Boolean, title: 'M'}, 
+	E: {type: Boolean, title: 'E'},
+        HIVStatus: {type: String, title: 'HIV Status'}, 
+	
+	CD4: {type: Number, title: 'CD4'}, 
+	
+        Elective: {type: Boolean, title: 'Elective'}, 
+	Emergency: {type: Boolean, title: 'Emergency'},
+	
+        G1: {type: Boolean, title: 'G1'}, 
+	G2: {type: Boolean, title: 'G2'}, 
+	G3: {type: Boolean, title: 'G3'}, 
+	ONC: {type: Boolean, title: 'ONC'},
+	
+	Oncology: {type: Boolean, title: 'Oncology'}, 
+	GeneralGynae: {type: Boolean, title: 'General Gynaecology'},
+	Infertility: {type: Boolean, title: 'Infertility'}, 
+	Urologynaecology: {type: Boolean, title: 'Urologynaecology'},
+	
+       OpenAbdomen: {type: Boolean, title: 'Open Abdomen'},
+       Endoscopy: {type: Boolean, title: 'Endoscopy'}, 
+       Vaginal: {type: Boolean, title: 'Vaginal'},
         PreOperICD10Codes: [{type: String, title: 'Pre-Operative Diagnosis ICD Code:'}],
-        PerformedBy: {Properties: {Consultant: {type: Boolean, title: 'Consultant'}, Fellow: {type: Boolean, title: 'Fellow'}, Registrar: {type: Boolean, title: 'Registrar'}, Intern: {type: Boolean, title: 'Intern'}}},
+	
+	Consultant: {type: Boolean, title: 'Consultant'}, 
+	Fellow: {type: Boolean, title: 'Fellow'}, 
+	Registrar: {type: Boolean, title: 'Registrar'},
+	Intern: {type: Boolean, title: 'Intern'},
+       
 	DygnosisNotOnICD10List: [{type: String, title: 'Diagnosis if not on ICD 10 list'}],
+	
 	ProPerformedCode: [{type: String, title: 'Procedure(s) Performed(Code) :'}],
-	IOComplications: {Properties: {None: {type: Boolean, title: 'None'}, BladderInjury: {type: Boolean, title: 'Bladder Injury'}, BowelInjury: {type: Boolean, title: 'Bowel Injury'}, BRUBT: {type: Boolean, title: 'BRUBT'}, ProcedureNotCompleted: {type: Boolean, title: 'Procedure Not Completed'}}},
+	
+	None: {type: Boolean, title: 'None'},
+	BladderInjury: {type: Boolean, title: 'Bladder Injury'}, 
+	BowelInjury: {type: Boolean, title: 'Bowel Injury'}, 
+	BRUBT: {type: Boolean, title: 'BRUBT'}, 
+	ProcedureNotCompleted: {type: Boolean, title: 'Procedure Not Completed'},
+	
 	OtherComplications: {type: String, title : 'Other Complications'}
 });
 
+var AdmissionDischarge = new Schema({
 
-mongoose.model('forms', Form);
-mongoose.model('users', Users);
-mongoose.model('patient', Patient);
-mongoose.model('statistics', Statistics);
-mongoose.model('gynaecologySurgery', GynaecologySurgery);
+        HospitalNumber: {type: String, require: true, title: 'Hospital Number'},
+	
+	DateOfBirth: {type: Date, require: true, title: 'Date of Birth' },
+	
+	Age: {type: Number, require: true, title: 'Age'},
+	
+	Name: {type: String, require: true, title: 'Name'},
+	
+        Surname: {type: String, require: true, title: 'Surname'},
+	
+	DateofAdmission: {type: Date, require: true, title: 'Date of Admission' },
+	
+	
+	G1: {type: Boolean, title: 'G1'}, 
+	G2: {type: Boolean, title: 'G2'}, 
+	G3: {type: Boolean, title: 'G3'}, 
+	ONC: {type: Boolean, title: 'ONC'},
+	
+
+	Emergency: {type: Boolean, title: 'Emergency'},
+	ElectiveSurgery: {type: Boolean, title: 'Elective Surgery'}, 
+	ElectiveOther: {type: Boolean, title: 'Elective Other'}, 
+	
+	P: {type: Boolean, title: 'P'}, 
+	G: {type: Boolean, title: 'G'}, 
+	M: {type: Boolean, title: 'M'}, 
+	E: {type: Boolean, title: 'E'},
+	
+        HIVStatus: {type: String, title: 'HIV Status'}, 
+	CD4: {type: Number, title: 'CD4'}, 
+	
+	
+	DateofDischarge: {type: Date, require: true, title: 'Date of Discharge' },
+	
+	TotalNumberOfDaysHospital: {type: Number, require: true, title: 'Total Number of Days in Hospital'},
+	
+	DaysInWard: {type: Number, require: true, title: 'Days in Ward'},
+	
+	DaysInICU: {type: Number, require: true, title: 'Days in ICU'},
+	
+	DaysInHighCare: {type: Number, require: true, title: 'Days in High Care'},
+	
+	FinalDiagnosisOnDischarge: [{type: String, title: 'Final diagnosis on discharge (ICD 10)'}],
+	
+	OtherComplications: {type: String, title : 'Other complications or reasons for non completion or anaesthetic complication '}
+});
+
+
+var forms = mongoose.model('forms', Form);
+var users = mongoose.model('users', Users);
+var patient =  mongoose.model('patient', Patient);
+var statistics = mongoose.model('statistics', Statistics);
+var gynaecologySurgery = mongoose.model('gynaecologySurgery', GynaecologySurgery);
+var addmissionDischarge = mongoose.model('addmissionDischarge', AdmissionDischarge);
+
+module.exports = { 
+	forms: forms,
+	users: users,
+	patient: patient,
+	statistics: statistics,
+	gynaecologySurgery: gynaecologySurgery,
+	addmissionDischarge: addmissionDischarge
+	};
